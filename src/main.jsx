a632457728f19bc32af72632b40ea852a7a6e70b
Prajakta244 <58784411+Prajakta244@users.noreply.github.com>
2025-03-07 20:09:30 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home.jsx'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Register from './pages/Register.jsx'
 import RegisterForm from './pages/RegisterForm.jsx'
+import DoctorInfo from './pages/DoctorInfo'
+import AppointmentDetails from './components/AppointmentDetails'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -13,6 +15,9 @@ createRoot(document.getElementById('root')).render(
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/register-clinic' element={<RegisterForm/>}/>
+          <Route path='/register-hospital' element={<RegisterForm/>}/>
+          <Route path='/doctor-info/:docId' element={<DoctorInfo/>}/>
+          <Route path='/appointment-details' element={<AppointmentDetails/>}/>
       </Routes>
   </BrowserRouter>,
 )
